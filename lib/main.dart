611d24f798c4e7732a1aa9fc84752ecfe0487e33
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-06-16 13:29:07 -0300

    Creo proyecto stacked

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:microdonations/app/app.bottomsheets.dart';
+import 'package:microdonations/app/app.dialogs.dart';
+import 'package:microdonations/app/app.locator.dart';
+import 'package:microdonations/app/app.router.dart';
+import 'package:microdonations/ui/common/app_colors.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+void main() {
+  setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: Theme.of(context).copyWith(
+        primaryColor: kcBackgroundColor,
+        focusColor: kcPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+            ),
+      ),
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [
+        StackedService.routeObserver,
+      ],
+    );
+  }
+}
