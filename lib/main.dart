0ffeea388b95e7fc6970a30180ee96bf4af32ceb
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-06-23 14:14:15 -0300

    Quitamos bandera de debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:microdonations/app/app.bottomsheets.dart';
@@ -46,6 +45,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: CustomStylesTheme.mainTheme,
       initialRoute: Routes.startupView,
       onGenerateRoute: StackedRouter().onGenerateRoute,
