1e415b30d08039a1cc986533080658c8a7184a0d
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-06-28 16:10:46 -0300

    Guardo en storage si se vio el onboarding

--- lib/ui/views/onboarding/onboarding_viewmodel.dart
+++ lib/ui/views/onboarding/onboarding_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:microdonations/app/app.router.dart';
+import 'package:microdonations/ui/common/helpers/storage.helpers.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -8,5 +9,10 @@ class OnboardingViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
   /// Navega a la pantalla de home y la hace root.
-  void navigateToHome() => _navigationService.replaceWithHomeView();
+  void navigateToHome() {
+    /// Marco que se vio el onboarding para que no se vuelva a mostrar.
+    StorageHelper.saveOnboarding(true);
+
+    _navigationService.replaceWithHomeView();
+  }
 }
