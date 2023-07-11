1316153666d72c74fbd1d738a0ec8583fe199b3d
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-07-11 12:30:18 -0300

    Creo metodo para actualizar usuario

--- lib/core/services/user_service.dart
+++ lib/core/services/user_service.dart
@@ -1,9 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:microdonations/core/models/update_user_request.model.dart';
+import 'package:microdonations/ui/common/helpers/logger.helpers.dart';
+
+import '../interceptor/dio.interceptor.dart';
 import '../models/logged_user.model.dart';
 
 class UserService {
+  final dio = DioClient().dio;
+
+  String? apiUrl = dotenv.env['API_URL'];
+
   late LoggedUser _loggedUser;
 
   void set(LoggedUser user) => _loggedUser = user;
 
   LoggedUser get loggedUser => _loggedUser;
+
+  Future<void> updateUser(UpdateUserRequest updateUserRequest) async {
+    try {
+      final response = await dio.put(
+        '${apiUrl}social-login',
+        data: updateUserRequest.toJson(),
+        options: Options(
+          responseType: ResponseType.json,
+        ),
+      );
+    } catch (e) {
+      logError('No se pudo actualizar el usuario! ${e.toString()}');
+    }
+  }
 }
