973954ebcd1402089cedca000c9280b385df022e
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-07-12 14:35:18 -0300

    Creo un servicio para manejar auth api

---
+++ lib/services/auth_api_service.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:microdonations/core/interceptor/dio.interceptor.dart';
+import 'package:microdonations/core/models/social_login_response.model.dart';
+
+class AuthApiService {
+  final dio = DioClient().dio;
+
+  final String? _apiUrl = dotenv.env['API_URL'];
+
+  /// Login para el usuario.
+  /// Envia el email y el token de Google.
+  Future<SocialLoginResponse> login(String email, String token) async {
+    try {
+      final response = await dio.post(
+        '${_apiUrl}social-login',
+        data: {
+          'email': email,
+          'token': token,
+        },
+        options: Options(
+          responseType: ResponseType.json,
+        ),
+      );
+
+      /// Parseo la respuesta de la API.
+      final socialLoginResp = SocialLoginResponse.createOne(
+        response.data['data'],
+      );
+
+      return socialLoginResp;
+    } on DioException catch (e) {
+      throw DioClient.parseDioException(e);
+    }
+  }
+}
