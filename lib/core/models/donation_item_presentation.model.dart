fbd5813ee32c838376eb69619ebcd12204951069
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-07-19 13:04:27 -0300

    Termino de maquetar modelo de donation item presentation

--- lib/core/models/donation_item_presentation.model.dart
+++ lib/core/models/donation_item_presentation.model.dart
@@ -1,17 +1,38 @@
+import 'package:microdonations/ui/common/helpers/logger.helpers.dart';
+
 class DonationItemPresentation {
-  final String label;
-  final String imgPath;
+  final int id;
+  final String text;
+  final String urlImg;
 
   DonationItemPresentation({
-    required this.label,
-    required this.imgPath,
+    required this.id,
+    required this.text,
+    required this.urlImg,
   });
 
   /// Crea una instancia de [DonationItemPresentation]
-  static DonationItemPresentation createOne(String label, imgPath) {
+  static DonationItemPresentation createOne(Map<String, dynamic> data) {
     return DonationItemPresentation(
-      label: label,
-      imgPath: imgPath,
+      id: data['id'],
+      text: data['text'],
+      urlImg: data['image'],
     );
+  }
+
+  /// Crea una lista de [DonationItemPresentation]
+  static List<DonationItemPresentation> createList(List<dynamic> data) {
+    List<DonationItemPresentation> items = [];
+
+    for (var course in data) {
+      try {
+        items.add(DonationItemPresentation.createOne(course));
+      } catch (e) {
+        logError(e);
+        continue;
+      }
+    }
+
+    return items;
   }
 }
