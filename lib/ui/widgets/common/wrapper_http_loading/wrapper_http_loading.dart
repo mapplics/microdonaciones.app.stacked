45a958b351d1cab05bd3c8186ae8b1413d10e4dd
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-07-20 14:51:30 -0300

    Creo un widget wrapper para manejar llamadas http

---
+++ lib/ui/widgets/common/wrapper_http_loading/wrapper_http_loading.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:microdonations/ui/common/app_theme.dart';
+import 'package:microdonations/ui/widgets/common/empty_state_with_button/empty_state_with_button.dart';
+import 'package:stacked/stacked.dart';
+
+import 'wrapper_http_loading_model.dart';
+
+/// Widget que permite resolver de manera ordenada la transicion
+/// de una llamada http.
+///
+/// Si [showLoading] es true el widget muestra un loading.
+///
+/// Si [showError] es true el widget muestra un empty state de error con un
+/// boton que ejecuta el [retryFunction].
+///
+/// [retryFunction] deberia volver a ejecutar la llamada Http que
+/// el widget padre ejecuto, para volver a empezar el flujo.
+///
+/// [mainContent] es el widget que se va a mostrar
+/// una vez que la llamada http fue exitosa y no hubo ningun error.
+class WrapperHttpLoading extends StackedView<WrapperHttpLoadingModel> {
+  final Widget mainContent;
+  final bool showLoading;
+  final bool showError;
+  final Function retryFunction;
+
+  const WrapperHttpLoading({
+    required this.mainContent,
+    required this.showLoading,
+    required this.showError,
+    required this.retryFunction,
+    super.key,
+  });
+
+  @override
+  Widget builder(
+    BuildContext context,
+    WrapperHttpLoadingModel viewModel,
+    Widget? child,
+  ) {
+    return (showLoading)
+        ? const Center(
+            child: CircularProgressIndicator(
+              color: CustomStylesTheme.primaryColor,
+            ),
+          )
+        : (showError)
+            ? EmptyStateWithButton(
+                title: 'Hago salio mal',
+                body: 'Por favor, volver a intentarlo.',
+                textButton: 'Volver a intentar',
+                buttonAction: retryFunction,
+              )
+            : mainContent;
+  }
+
+  @override
+  WrapperHttpLoadingModel viewModelBuilder(
+    BuildContext context,
+  ) =>
+      WrapperHttpLoadingModel();
+}
