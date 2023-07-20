fc2773bb5a8842535de2977289176792164ea0e8
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-07-20 14:51:40 -0300

    Utilizo el wrapper http en el flujo de donacion

--- lib/ui/widgets/common/donation_items_selector/donation_items_selector.dart
+++ lib/ui/widgets/common/donation_items_selector/donation_items_selector.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:microdonations/ui/common/app_theme.dart';
 import 'package:microdonations/ui/widgets/common/donation_item_card/donation_item_card.dart';
-import 'package:microdonations/ui/widgets/common/empty_state_with_button/empty_state_with_button.dart';
+import 'package:microdonations/ui/widgets/common/wrapper_http_loading/wrapper_http_loading.dart';
 import 'package:stacked/stacked.dart';
 
 import 'donation_items_selector_model.dart';
@@ -15,54 +14,34 @@ class DonationItemsSelector extends StackedView<DonationItemsSelectorModel> {
     DonationItemsSelectorModel viewModel,
     Widget? child,
   ) {
-    return (viewModel.isLoading)
-        ? const Center(
-            child: CircularProgressIndicator(
-              color: CustomStylesTheme.primaryColor,
-            ),
-          )
-        : (viewModel.httpHaveFailed)
-            ? EmptyStateWithButton(
-                title: 'Hago salio mal',
-                body: 'Por favor, volver a intentarlo.',
-                textButton: 'Volver a intentar',
-                buttonAction: viewModel.getDonationsItems,
-              )
-            : Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 19.0),
-                    child: Text(
-                      'Seleccioná que alimentos son los que vas a donar',
-                      style: CustomStylesTheme.regular14_16,
+    return WrapperHttpLoading(
+      showLoading: viewModel.isLoading,
+      showError: viewModel.httpHaveFailed,
+      retryFunction: viewModel.getDonationsItems,
+      mainContent: SingleChildScrollView(
+        child: Column(
+          children: [
+            ...viewModel.donationItemsOptions
+                .map(
+                  (item) => Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 7.5,
+                    ),
+                    child: DonationItemCard(
+                      product: item,
+                      initialValue: viewModel.isSelectedItem(item),
+                      onChange: (newValue) => viewModel.handleToggleDonation(
+                        newValue,
+                        item,
+                      ),
                     ),
                   ),
-                  SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        ...viewModel.donationItemsOptions
-                            .map(
-                              (item) => Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 7.5,
-                                ),
-                                child: DonationItemCard(
-                                  product: item,
-                                  initialValue: viewModel.isSelectedItem(item),
-                                  onChange: (newValue) =>
-                                      viewModel.handleToggleDonation(
-                                    newValue,
-                                    item,
-                                  ),
-                                ),
-                              ),
-                            )
-                            .toList(),
-                      ],
-                    ),
-                  ),
-                ],
-              );
+                )
+                .toList(),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
