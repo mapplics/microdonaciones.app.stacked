7a72e81b5a6da5024661f5b77b0f77909cf1a0bf
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-07-21 15:20:39 -0300

    Paso logica de donation service a NewDonation model

--- lib/core/models/new_donations.model.dart
+++ lib/core/models/new_donations.model.dart
@@ -1,12 +1,62 @@
 import 'package:microdonations/core/models/donation_item.model.dart';
+import 'package:microdonations/core/models/product.model.dart';
+import 'package:microdonations/ui/common/helpers/logger.helpers.dart';
+import 'package:microdonations/ui/widgets/common/delivery_segmented_buttons/delivery_segmented_buttons_model.dart';
+import 'package:collection/collection.dart';
 
 class NewDonation {
-  List<DonationItem> donationsList;
+  TypeDelivery typeDelivery;
 
   NewDonation({
-    required this.donationsList,
-  }) : assert(
-          donationsList.isNotEmpty,
-          'DonationList should not be empty',
-        );
+    this.typeDelivery = TypeDelivery.branch,
+  });
+
+  final List<DonationItem> _donationsList = [];
+
+  /// Devuelve la lista de items [DonationItem] que el usuario selecciono
+  /// para donar.
+  List<DonationItem> get donationsItemsList => _donationsList;
+
+  /// Recibe un [DonationItem] y lo agrega a [_donationsList]
+  void addDonationItem(DonationItem donationItem) {
+    _donationsList.add(donationItem);
+  }
+
+  /// Recibe un [Product] que se utiliza para buscar una
+  /// coincidencia en [_donationsList] si se encuentra una coincidencia
+  /// se elimina de la lista de [_donationsList]
+  void removeDonationItem(Product product) {
+    final _index = _donationsList.indexWhere(
+      (selectedItem) => selectedItem.product.id == product.id,
+    );
+
+    _donationsList.removeAt(_index);
+  }
+
+  /// Actualiza el tipo de delivery [TypeDelivery] de la donacion
+  set updateTypeDelivery(TypeDelivery type) => typeDelivery = type;
+
+  /// Recibe un [DonationItem] y la cantidad [quantity] que se le va a setear
+  /// al mismo [DonationItem].
+  void updateDonationItemQuality(DonationItem item, int quantity) {
+    final _found = donationsItemsList.firstWhereOrNull(
+      (selectedItem) => selectedItem.title == item.title,
+    );
+
+    /// Chequeo si encontre el item que quiero modificar
+    if (_found != null) {
+      _found.updateQuantity = quantity;
+    } else {
+      logError('DonationItem not found!');
+    }
+  }
+
+  /// Devuelve true si existe un [DonationItem]
+  /// en [_donationsList] para el [product].
+  bool checkIfItemExist(Product product) {
+    final _found = donationsItemsList.firstWhereOrNull(
+        (selectedItem) => selectedItem.product.id == product.id);
+
+    return (_found != null);
+  }
 }
