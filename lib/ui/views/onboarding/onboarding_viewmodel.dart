b7372f418d61234a4d52603ca93877a17414dbdf
Gonzalo Etchegaray <xetchegaraygonzalo@gmail.com>
2023-08-14 16:28:49 -0300

    Refactor de onboarding. Muevo logica a viewmodel

--- lib/ui/views/onboarding/onboarding_viewmodel.dart
+++ lib/ui/views/onboarding/onboarding_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:microdonations/app/app.router.dart';
 import 'package:microdonations/ui/common/helpers/storage.helpers.dart';
 import 'package:stacked/stacked.dart';
@@ -7,6 +8,23 @@ import '../../../app/app.locator.dart';
 
 class OnboardingViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final PageController _pageController = PageController(initialPage: 0);
+  final int _numSlides = 3;
+  int _currentPage = 0;
+
+  /// Devuelve la cantidad total de slides.
+  int get numSlides => _numSlides;
+
+  /// Devuelve la pagina actual del slide que se esta mostrando.
+  int get currentPage => _currentPage;
+
+  PageController get pageController => _pageController;
+
+  /// Actualiza la referencia de [_currentPage]
+  void onPageChange(int page) {
+    _currentPage = page;
+    rebuildUi();
+  }
 
   /// Navega a la pantalla de home y la hace root.
   void navigateToHome() {
@@ -15,4 +33,7 @@ class OnboardingViewModel extends BaseViewModel {
 
     _navigationService.replaceWithHomeView();
   }
+
+  /// Devuelve true si la slide que se esta mostrando es la ultima.
+  bool get isLastPage => (_currentPage != (_numSlides - 1));
 }
