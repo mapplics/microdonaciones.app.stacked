076efc144c6da8d0ff36a21e47d80b0d205e3dd0
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-10-23 18:36:54 -0300

    Rediseño card de ong

--- lib/ui/widgets/common/ong_selector_card/ong_selector_card.dart
+++ lib/ui/widgets/common/ong_selector_card/ong_selector_card.dart
@@ -22,64 +22,133 @@ class OngSelectorCard extends StackedView<OngSelectorCardModel> {
     Widget? child,
   ) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
       decoration: const BoxDecoration(
         color: AppTheme.gray400,
         borderRadius: BorderRadius.all(
           Radius.circular(10),
         ),
       ),
-      child: Column(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 6.0),
-                child: Text(
-                  ongModel.name,
-                  style: AppTheme.bold16_24,
-                ),
+      child: IntrinsicHeight(
+        child: Row(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 8.0),
+              width: 4,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.horizontal(left: Radius.circular(5)),
+                color: AppTheme.tertiaryColor,
               ),
-              if (ongModel.description != null)
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: Text(
-                    ongModel.description!,
-                    style: AppTheme.regular14_16,
-                  ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                  bottom: 8.0,
+                  right: 12.0,
                 ),
-            ],
-          ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 6.0),
+                      child: Text(
+                        ongModel.name,
+                        style: AppTheme.bold16_24.copyWith(
+                          color: AppTheme.tertiaryColor,
+                        ),
+                      ),
+                    ),
 
-          /// Botones
-          Row(
-            children: [
-              CustomOutlineButton(
-                icon: const Icon(
-                  PhosphorIcons.mapPinLineLight,
-                  color: AppTheme.tertiaryColor,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (ongModel.description?.isNotEmpty ?? false)
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 10.0),
+                              child: RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Descripión: ',
+                                      style: AppTheme.bold14_16.copyWith(
+                                        color: AppTheme.blackColor,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: ongModel.description,
+                                      style: AppTheme.regular14_20.copyWith(
+                                        color: AppTheme.blackColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          if (ongModel.address?.isNotEmpty ?? false)
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 10.0),
+                              child: RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Dirección: ',
+                                      style: AppTheme.bold14_16.copyWith(
+                                        color: AppTheme.blackColor,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: ongModel.address,
+                                      style: AppTheme.regular14_20.copyWith(
+                                        color: AppTheme.blackColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+
+                    /// Botones
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 15.0),
+                          child: CustomOutlineButton(
+                            icon: const Icon(
+                              PhosphorIcons.mapPinLineLight,
+                              color: AppTheme.tertiaryColor,
+                            ),
+                            label: 'Ver ubicacion',
+                            action: () async {
+                              try {
+                                await LaunchUrlHelper.openUrl(
+                                    ongModel.googleLink!);
+                              } catch (e) {
+                                MessegeHelper.showErrorSnackBar(
+                                  context,
+                                  'No se pudo abrir la ubicación. Volvé a intentarlo mas tarde.',
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                        CustomFillButton(
+                          label: 'Donar',
+                          action: () => viewModel.goToDonate(ongModel),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-                label: 'Ver ubicacion',
-                action: () async {
-                  try {
-                    await LaunchUrlHelper.openUrl(ongModel.googleLink!);
-                  } catch (e) {
-                    MessegeHelper.showErrorSnackBar(
-                      context,
-                      'No se pudo abrir la ubicación. Volvé a intentarlo mas tarde.',
-                    );
-                  }
-                },
               ),
-              const Spacer(),
-              CustomFillButton(
-                label: 'Donar',
-                action: () => viewModel.goToDonate(ongModel),
-              )
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
