3e1b44f26870b1c345587f9d2636575d8ce6eae2
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2023-10-26 14:42:59 -0300

    Actualizo card de punto de entrega

--- lib/ui/widgets/common/reception_point_list/reception_point_list.dart
+++ lib/ui/widgets/common/reception_point_list/reception_point_list.dart
@@ -38,7 +38,7 @@ class ReceptionPointList extends StackedView<ReceptionPointListModel> {
                   ),
                 ),
                 subtitle: Text(
-                  'Detalles: ${receptionPoint.extraInfo}',
+                  receptionPoint.extraInfo,
                   style: AppTheme.regular14_20.copyWith(
                     color: AppTheme.blackColor,
                   ),
