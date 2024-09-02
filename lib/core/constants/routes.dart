91c8bdd2c1107799519315744fdf313d411bcc9e
Etchegaray Gonzalo <44512291+EtcGonza@users.noreply.github.com>
2024-09-02 20:24:40 -0300

    Constantes para rutas

---
+++ lib/core/constants/routes.dart
@@ -0,0 +1,7 @@
+class RoutesData {
+  /// Devuelve la ruta de la pantalla de detalle de inventario
+  static String get newDonationViewRoute => '/new-donation-view';
+
+  /// Devuelve la ruta de la pantalla de detalle de inventario
+  static String get homeViewRoute => '/home-view';
+}
